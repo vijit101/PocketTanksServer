603fcb620e9d63c661d9f6dc4b9345780676d562
Shubham <40598861+vijit101@users.noreply.github.com>
2019-12-01 23:07:18 +0530

    -Added Player Authentication Code

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var app = require('express');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-
+var MyPlayer = require('./Scripts/Player/Player.js')
+var player = MyPlayer.Player;
+//connection
 io.on('connection',function(socket){
     console.log("Server Connection Established");
-    socket.emit('connection');
+    var newPlayer = new player(socket);
+    newPlayer.init();
+    //socket.emit('connection',{"Id":100});
 });
+//Authentication
 
 server.listen(3000,"127.0.0.1",function(){
     console.log("Listening")
