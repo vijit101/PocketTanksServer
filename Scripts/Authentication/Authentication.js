d33708f1a7361a0658bb8edd4b0fc2b40f471333
Shubham <40598861+vijit101@users.noreply.github.com>
2019-12-10 07:28:49 +0530

    -Changed a bit of Auth Logic

--- Scripts/Authentication/Authentication.js
+++ Scripts/Authentication/Authentication.js
@@ -7,9 +7,24 @@ class Authentication{
         console.log("Authentication + constructor")
         this.socket = socket;
         this.state = AuthState.Auth_Pending;
+        this.PlayerID = null;
     }
     init(){
-        this.socket.on(EventStrings.AuthenticationRequest,this.OnAuthRequest)
+        this.socket.on(EventStrings.AuthenticationRequest,(Data)=>{
+            this.state = AuthState.Auth_Progress;
+            console.log("AuthenticationRequest + Authentication + OnAuthRequest "+this.state + Data['PlayerID']);
+            if(Data['PlayerID'] == 0)
+            {
+                console.log("check from onAuth")
+                this.PlayerID = Data['PlayerID'];
+                this.socket.emit(EventStrings.AuthenticationResponse)
+            }
+            else
+            {
+                this.PlayerID = Math.ceil(Math.random()*100);
+                this.socket.emit(EventStrings.AuthenticationResponse)
+            }
+        })
         console.log("Authentication + init "+this.state );
     }
     OnAuthRequest(Data)
@@ -18,19 +33,17 @@ class Authentication{
         console.log("AuthenticationRequest + Authentication + OnAuthRequest "+this.state + Data['PlayerID']);
         if(Data['PlayerID'] == 0)
         {
-            var PlayerID = Math.ceil(Math.random()*100);
-            this.socket.emit(EventStrings.AuthenticationResponse,PlayerID);
-            console.log("AuthenticationRequest + Authentication + OnAuthRequest "+this.state + Data['PlayerID']);
-
+            console.log("check from onAuth")
+            this.PlayerID = Data['PlayerID'];
+            this.socket.emit(EventStrings.AuthenticationResponse)
         }
         else
         {
-            var PlayerID = 0;
-            this.socket.emit(EventStrings.AuthenticationResponse,PlayerID);
-            console.log("AuthenticationRequest + Authentication + OnAuthRequest "+this.state + Data['PlayerID']);
-
+            this.PlayerID = Math.ceil(Math.random()*100);
+            this.socket.emit(EventStrings.AuthenticationResponse)
         }
     }
+
     OnAuthFailed()
     {
         this.state = AuthState.Auth_Failed;
