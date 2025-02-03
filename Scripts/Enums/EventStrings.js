038f888c43b4fca1336d3da8fc2280784a7ff6c6
Shubham <40598861+vijit101@users.noreply.github.com>
2025-02-03 18:25:40 +0530

    Update EventStrings.js

--- Scripts/Enums/EventStrings.js
+++ Scripts/Enums/EventStrings.js
@@ -16,3 +16,5 @@ const EventStrings = {
     HealthFromP2 :"HealthFromP2",
 }
 module.exports = {EventStrings:EventStrings};
+
+// this was kept as a mirror of event strings used in the unity frontend , as i faced problems to always wrtie correct spellings for the events this way it was fast to reuse certain events without bothering to match it up to the unity logics 
